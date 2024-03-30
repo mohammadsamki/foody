fa458fd1cabfa675cc94b729cc7fc8343b41771a
mohammed alsamaki <samkimohd@gmail.com>
2024-03-31 01:54:52 +0300

    register

---
+++ js/register.js
@@ -0,0 +1,23 @@
+var registerForm = document.getElementById('registerForm');
+var userName = document.getElementById('form3Example1c');
+var email = document.getElementById('form3Example3c');
+var password = document.getElementById('form3Example4c');
+var confirmPassword = document.getElementById('form3Example4cd');
+var dataFromStorage = JSON.parse(localStorage.getItem('userDataFromLocal'));
+
+var allUsers=dataFromStorage
+registerForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    var userData={
+        username:userName.value,
+        email:email.value,
+        password:password.value,
+        confirmPassword:confirmPassword.value
+    }
+    allUsers.push(userData);
+    console.log(allUsers);
+    localStorage.setItem('userDataFromLocal',JSON.stringify(allUsers));
+
+    
+})
